0e26e25e21ddabfad75aafcb4876f8eee2416d20
Dimas <dgusti41@gmail.com>
2025-11-11 23:47:08 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius excepturi sunt nostrum quas voluptatem molestiae repellendus ducimus officia vero tempore reprehenderit repudiandae maxime fuga expedita odit natus illum, id consectetur!</p>
 </body>
 </html>
